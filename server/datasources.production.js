3b5ca618b82fe13c59adc86014b495a3766517b2
Michael Fischer <zarkal@live.com>
2019-08-06 11:42:31 -0700

    Fixes mongodb url for heroku environment

---
+++ server/datasources.production.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "MongoDB": {
+        "host": "",
+        "port": 0,
+        "url": process.env.MONGODB_URI,
+        "database": "lb-workshop-db",
+        "password": "",
+        "name": "MongoDB",
+        "user": "",
+        "useNewUrlParser": true,
+        "connector": "mongodb"
+    }
+}
